1d3d22cc7f499392025fac6d834967bc876226de
Ondřej Šelder <seldeond@cvut.cz>
2019-09-05 13:41:46 +0200

    Refactoring - Functions' templates, TemplateRenderer, TemplateModel renamed

--- DasContract.Blockchain.Plutus/Properties/Resources.Designer.cs
+++ DasContract.Blockchain.Plutus/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace DasContract.Blockchain.Plutus.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace DasContract.Blockchain.Plutus.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DasContract.Blockchain.Plutus.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace DasContract.Blockchain.Plutus.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -69,17 +69,27 @@ namespace DasContract.Blockchain.Plutus.Properties {
         ///--
         ///--
         ///{% for function in p.WalletFunctions %}
-        ///{{ function.Name }} :: {{ function.Head }}
-        ///{{ function.Name }} = {{ function.Body }}
+        ///{{ function.GeneratedFunction }}
+        ///
         ///{% endfor %}
         ///
         ///{% if p.WalletFunctions.size != 0 %}
         ///$(mkFunctions[&apos;{% assign functionNames = p.WalletFunctions | map: &apos;Name&apos; %}{{ functionNames | join: &quot;, &apos;&quot; }}])
         ///{% endif %}.
         /// </summary>
-        internal static string FluidSmartContractTemplate {
+        public static string FluidSmartContractStructure {
             get {
-                return ResourceManager.GetString("FluidSmartContractTemplate", resourceCulture);
+                return ResourceManager.GetString("FluidSmartContractStructure", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {{ p.Name }} :: MonadWallet m =&gt; m()
+        ///{{ p.Name }} = logMsg &quot;{{ p.Message }}&quot;.
+        /// </summary>
+        public static string LogAMessage {
+            get {
+                return ResourceManager.GetString("LogAMessage", resourceCulture);
             }
         }
     }
---
+++ DasContract.Blockchain.Plutus/TemplateRenderer.cs
@@ -0,0 +1,20 @@
+﻿using Fluid;
+
+namespace DasContract.Blockchain.Plutus
+{
+    public static class TemplateRenderer
+    {
+        public static string Assemble ( string templateSourceCode, object templateModel )
+        {
+            if (FluidTemplate.TryParse(templateSourceCode, out var template))
+            {
+                var context = new TemplateContext();
+                context.MemberAccessStrategy.Register(templateModel.GetType());
+                context.SetValue("p", templateModel);
+
+                return template.Render(context);
+            }
+            return null;
+        }
+    }
+}
