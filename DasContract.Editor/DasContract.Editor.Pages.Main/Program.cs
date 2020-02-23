a8c5de50c405e4722d99449ca2876adddafec760
Martin Drozdík <drozdik.m@seznam.cz>
2020-02-23 12:07:47 +0100

    Prepare project for front-end

---
+++ DasContract.Editor/DasContract.Editor.Pages.Main/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DasContract.Editor.Pages.Main
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            //builder.Services.Add
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
