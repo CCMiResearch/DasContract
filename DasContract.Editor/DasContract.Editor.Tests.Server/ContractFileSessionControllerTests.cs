ed6589c0255e897e8474f8c8a0dade0990d8baad
Martin Drozdík <drozdik.m@seznam.cz>
2020-02-25 18:18:11 +0100

    Prepare server integration testing

---
+++ DasContract.Editor/DasContract.Editor.Tests.Server/ContractFileSessionControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DasContract.Editor.Tests.Server.ServerFactory;
+using Xunit;
+
+namespace DasContract.Editor.Tests.Server
+{
+    public class ContractFileSessionControllerTests: IClassFixture<ServerWebApplicationFactory>
+    {
+        readonly ServerWebApplicationFactory factory;
+
+        public ContractFileSessionControllerTests(ServerWebApplicationFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var client = factory.CreateClient();
+
+            var response = client.GetAsync("xxx");
+
+            //...
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DasContract.Editor/DasContract.Editor.Tests.Server/ServerFactory/ServerWebApplicationFactory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Linq;
+using DasContract.Editor.DataPersistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using DasContract.Editor.Server;
+
+namespace DasContract.Editor.Tests.Server.ServerFactory
+{
+    public class ServerWebApplicationFactory
+        : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                //Remove the original ContractEditorDb
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<ContractEditorDb>));
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                //Add InMemory database
+                services.AddDbContext<ContractEditorDb>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting-" + Guid.NewGuid().ToString());
+                });
+
+                //Build the service provider.
+                var sp = services.BuildServiceProvider();
+
+                //Create a scope to obtain a reference to the database context
+                using var scope = sp.CreateScope();
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<ContractEditorDb>();
+                var logger = scopedServices
+                    .GetRequiredService<ILogger<ServerWebApplicationFactory>>();
+
+                //Ensure the database is created.
+                db.Database.EnsureCreated();
+
+                try
+                {
+                    db.SeedTests();
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred seeding the " +
+                        "database with test messages. Error: {Message}", ex.Message);
+                }
+            });
+        }
+    }
+}
