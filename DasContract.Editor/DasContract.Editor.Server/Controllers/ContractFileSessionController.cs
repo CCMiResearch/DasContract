a8c4f2ad8685a19f62cf70056febafeac7eaae81
Martin Drozdík <drozdik.m@seznam.cz>
2020-02-25 17:43:18 +0100

    Finish untested facade

---
+++ DasContract.Editor/DasContract.Editor.Server/Controllers/ContractFileSessionController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DasContract.Editor.AppLogic.Facades;
+using DasContract.Editor.DataPersistence.Entities;
+using DasContract.Editor.Interfaces.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DasContract.Editor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractFileSessionController : ControllerBase
+    {
+        readonly ContractFileSessionFacade facade;
+
+        public ContractFileSessionController(ContractFileSessionFacade facade)
+        {
+            this.facade = facade;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ContractFileSession>>> GetAsync()
+        {
+            return Ok(await facade.GetAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ContractFileSession>> GetAsync(string id)
+        {
+            try
+            {
+                return Ok(await facade.GetAsync(id));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(string id)
+        {
+            try
+            {
+                await facade.DeleteAsync(id);
+                return Ok();
+            }
+            catch (NotFoundException e)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> InsertAsync(ContractFileSession item)
+        {
+            try
+            {
+                await facade.InsertAsync(item);
+                return Ok();
+            }
+            catch (AlreadyExistsException e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateAsync(ContractFileSession item)
+        {
+            try
+            {
+                await facade.UpdateAsync(item);
+                return Ok();
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (BadRequestException e)
+            {
+                return BadRequest(e);
+            }
+        }
+    }
+}
