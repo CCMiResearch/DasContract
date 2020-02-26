1f89ea2ae3bc8258827eb5fac4d6e0da1957bcdc
Martin Drozdík <drozdik.m@seznam.cz>
2020-02-26 14:30:19 +0100

    Setup initial page structure

---
+++ DasContract.Editor/DasContract.Editor.Pages.Main/Pages/ContractFileSessionPages/ContractFileSessionIndex.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bonsai.RazorComponents.MaterialBootstrap.Components.CBreadcrumbs;
+
+namespace DasContract.Editor.Pages.Main.Pages.ContractFileSessionPages
+{
+    public partial class ContractFileSessionIndex: PageBase
+    {
+        public static Breadcrumb Breadcrumb { get; } = new Breadcrumb("File session", "/ContractFileSession");
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Breadcrumbs
+                .AddHome("DasContract")
+                .AddLastCrumb(Breadcrumb);
+        }
+    }
+}
---
+++ DasContract.Editor/DasContract.Editor.Pages.Main/Pages/Index.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DasContract.Editor.Pages.Main.Pages
+{
+    public partial class Index: PageBase
+    {
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Breadcrumbs
+                .AddHome("DasContract")
+                .Finish();
+        }
+    }
+}
