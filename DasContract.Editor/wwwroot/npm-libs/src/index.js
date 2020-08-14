c838105b4decd5f0cc6e0dbec0cb2fdc595cd54e
Jan Klicpera <klicpjan@fit.cvut.cz>
2020-08-14 17:58:23 +0200

    Basic project setup with bpmn-js

---
+++ DasContract.Editor/wwwroot/npm-libs/src/index.js
@@ -0,0 +1,10 @@
+﻿import Modeler from 'bpmn-js/lib/Modeler';
+
+// create a modeler
+export function CreateModeler() {
+    var editor = new Modeler({
+        container: document.getElementById('canvas')
+    });
+
+    editor.createDiagram();
+}
