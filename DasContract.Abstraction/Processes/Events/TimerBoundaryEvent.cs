6a273faa50e18ac0d83e4724742cce3fdd49ff27
Jan Klicpera <klicpjan@cvut.cz>
2020-09-27 20:32:37 +0200

    Add comment explaining timer definition in timer boundary event

--- DasContract.Abstraction/Processes/Events/TimerBoundaryEvent.cs
+++ DasContract.Abstraction/Processes/Events/TimerBoundaryEvent.cs
@@ -14,7 +14,9 @@ namespace DasContract.Abstraction.Processes.Events
     {
         public TimerDefinitionType TimerDefinitionType { get; set; }
         /// <summary>
-        /// TODO
+        /// Contains timer definition data based on the TimerDefinitionType
+        /// For duration, the definition is in ISO 8601 Durations format.
+        /// For date, the definition is in ISO 8601 Dates format.
         /// </summary>
         public string TimerDefinition { get; set; }
     }
