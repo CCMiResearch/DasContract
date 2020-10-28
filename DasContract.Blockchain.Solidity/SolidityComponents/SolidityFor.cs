7c57bd36eabc1c133e730f0e239c7f7912b58ff4
Jan Klicpera <klicpjan@fit.cvut.cz>
2020-10-28 09:51:05 +0100

    Add Solidity for component

---
+++ DasContract.Blockchain.Solidity/SolidityComponents/SolidityFor.cs
@@ -0,0 +1,49 @@
+﻿using Liquid.NET;
+using Liquid.NET.Constants;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DasContract.Blockchain.Solidity.SolidityComponents
+{
+    public class SolidityFor: SolidityComponent
+    {
+        LiquidTemplate template = LiquidTemplate.Create("{{indent}}(uint {{varName}} = 0; {{varName}} < {{loopCount}}; {{varName++}}){ \n" +
+            "{{body}} \n" +
+            "{{indent}}}").LiquidTemplate;
+
+        string loopVariableName;
+        string loopCountVariable;
+
+        SolidityStatement body;
+
+        public SolidityFor(string loopVariableName, string loopCountVariable)
+        {
+            body = new SolidityStatement();
+            this.loopVariableName = loopVariableName;
+            this.loopCountVariable = loopCountVariable;
+        }
+
+        public void AddToBody(SolidityStatement statement)
+        {
+            body.Add(statement);
+        }
+
+        public override LiquidString ToLiquidString(int indent)
+        {
+            return LiquidString.Create(ToString(indent));
+        }
+
+        public override string ToString(int indent = 0)
+        {
+            ITemplateContext ctx = new TemplateContext();
+            ctx.DefineLocalVariable("indent", CreateIndent(indent)).
+                DefineLocalVariable("varName", LiquidString.Create(loopVariableName)).
+                DefineLocalVariable("loopCount", LiquidString.Create(loopCountVariable)).
+                DefineLocalVariable("body", body.ToLiquidString(indent + 1));
+            return template.Render(ctx).Result;
+        }
+
+    }
+}
+
