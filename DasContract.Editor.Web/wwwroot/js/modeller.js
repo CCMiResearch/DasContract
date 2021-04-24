881d2d17a98ab5a9e7bcd3972e4bbbf86b6a4c3b
Jan Klicpera <klicpjan@fit.cvut.cz>
2021-04-24 21:23:23 +0200

    Hide unused menu elements inside bpmn editor

--- DasContract.Editor.Web/wwwroot/js/modeller.js
+++ DasContract.Editor.Web/wwwroot/js/modeller.js
@@ -1,4 +1,16 @@
 ﻿import Modeler from 'bpmn-js/lib/Modeler';
+import PaletteProvider from 'bpmn-js/lib/features/palette/PaletteProvider';
+import {
+    START_EVENT,
+    INTERMEDIATE_EVENT,
+    END_EVENT,
+    BOUNDARY_EVENT,
+    TASK,
+    GATEWAY,
+    PARTICIPANT 
+} from 'bpmn-js/lib/features/replace/ReplaceOptions';
+import ReplaceMenuProvider from 'bpmn-js/lib/features/popup-menu/ReplaceMenuProvider';
+
 
 export function hookEvents() {
     console.log("Hooking events");
@@ -42,7 +54,6 @@ export function updateElementName(elementId, elementName) {
     modeling.updateProperties(element, { name: elementName});
 }
 
-
 function copyEventInformation(e) {
     let eventObj = { type: e.type };
     if (e.newId != null)
@@ -63,6 +74,7 @@ function copyEventInformation(e) {
     }
     return eventObj;
 }
+
 // create a modeler
 export function createModeler() {
     window.modeler = new Modeler({
@@ -73,14 +85,90 @@ export function createModeler() {
     });
     hookEvents();
     window.modeler.createDiagram();
+
+    removeUnusedReplaceMenuItems();
 }
 
+
+//Removes unused palette entries
+var _getPaletteEntries = PaletteProvider.prototype.getPaletteEntries;
+PaletteProvider.prototype.getPaletteEntries = function (element) {
+    var entries = _getPaletteEntries.call(this, element);
+    console.log(entries);
+    delete entries['create.data-store'];
+    delete entries['create.subprocess-expanded'];
+    delete entries['create.group'];
+    delete entries['create.data-object'];
+    return entries;
+}
+
+//Removes unused loop entry
+var _getLoopEntries = ReplaceMenuProvider.prototype._getLoopEntries;
+ReplaceMenuProvider.prototype._getLoopEntries = function (element) {  
+    let loopEntries = _getLoopEntries.call(this, element);
+    for (var i = loopEntries.length - 1; i >= 0; --i) {
+        if (loopEntries[i].id == "toggle-loop") {
+            loopEntries.splice(i, 1);
+        }
+    }
+    return loopEntries;
+}
+
+function removeUnusedReplaceMenuItems() {
+    removeUnusedReplaceMenuItem(TASK, UsedReplaceTaskItems);
+    removeUnusedReplaceMenuItem(START_EVENT, UsedReplaceStartEventItems);
+    removeUnusedReplaceMenuItem(END_EVENT, UsedReplaceEndEventItems);
+    removeUnusedReplaceMenuItem(BOUNDARY_EVENT, UsedReplaceBoundaryEventItems);
+    removeUnusedReplaceMenuItem(INTERMEDIATE_EVENT, UsedReplaceIntermediateEventItems);
+    removeUnusedReplaceMenuItem(GATEWAY, UsedReplaceGatewayItems);
+    removeUnusedReplaceMenuItem(PARTICIPANT, []);
+}
+
+function removeUnusedReplaceMenuItem(options, optionsToKeep) {
+    for (var i = options.length - 1; i >= 0; --i) {
+        if (!optionsToKeep.includes(options[i].label)) {
+            options.splice(i, 1);
+        }
+    }
+}
+
+var UsedReplaceTaskItems = [
+    'Task',
+    'User Task',
+    'Business Rule Task',
+    'Service Task',
+    'Script Task',
+    'Call Activity',
+]
+
+var UsedReplaceStartEventItems = [
+    'Start Event',
+    'End Event'
+]
+
+var UsedReplaceIntermediateEventItems = [
+    'Start Event',
+    'End Event',
+]
+
+var UsedReplaceEndEventItems = [
+    'Start Event',
+    'End Event',
+]
+
+var UsedReplaceBoundaryEventItems = [
+    'Timer Boundary Event'
+]
+
+var UsedReplaceGatewayItems = [
+    'Exclusive Gateway',
+    'Parallel Gateway'
+]
 
 
 export function setEventHandlerInstance(dotNetObjectRef) {
     modellerLib.eventHandlerInstanceRef = dotNetObjectRef;
 }
-
 
 export async function getDiagramXML() {
     var xml = (await window.modeler.saveXML({ format: true })).xml;
