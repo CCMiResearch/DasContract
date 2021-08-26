e78df7d8674a0c82749964225e60a1e8f7c72750
Jan Klicpera <klicpjan@fit.cvut.cz>
2021-08-26 19:34:04 +0200

    Remove static files from the repo

--- DasContract.Editor.Web/wwwroot/dist/dist_node/bpmn-js/lib/features/auto-place/index.js
+++
@@ -1,9 +0,0 @@
-import AutoPlaceModule from 'diagram-js/lib/features/auto-place';
-
-import BpmnAutoPlace from './BpmnAutoPlace';
-
-export default {
-  __depends__: [ AutoPlaceModule ],
-  __init__: [ 'bpmnAutoPlace' ],
-  bpmnAutoPlace: [ 'type', BpmnAutoPlace ]
-};
--- DasContract.Editor.Web/wwwroot/dist/dist_node/bpmn-js/lib/features/di-ordering/index.js
+++
@@ -1,8 +0,0 @@
-import BpmnDiOrdering from '../di-ordering/BpmnDiOrdering';
-
-export default {
-  __init__: [
-    'bpmnDiOrdering'
-  ],
-  bpmnDiOrdering: [ 'type', BpmnDiOrdering ]
-};
