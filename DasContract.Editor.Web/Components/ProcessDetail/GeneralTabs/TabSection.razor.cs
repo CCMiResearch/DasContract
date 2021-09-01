c25ddd5553c4e49da7646db00da935bc83a8ee98
Jan Klicpera <klicpjan@fit.cvut.cz>
2021-09-01 14:38:30 +0200

    Split general tab into sections

---
+++ DasContract.Editor.Web/Components/ProcessDetail/GeneralTabs/TabSection.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DasContract.Editor.Web.Components.ProcessDetail.GeneralTabs
+{
+    public partial class TabSection: ComponentBase
+    {
+        [Parameter]
+        public string SectionName { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+    }
+}
